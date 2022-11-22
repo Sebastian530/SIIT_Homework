f5094da8ea8c176cc95810ac4ec92bb06fc2687a
Sebastian530 <polgarsebastian@yahoo.com>
2022-11-22 20:20:49 +0200

    Add new line

--- src/main/java/org/siit/homework/week6/Main.java
+++ src/main/java/org/siit/homework/week6/Main.java
@@ -3,5 +3,6 @@ package org.siit.homework.week6;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the main class for week 6 homework");
+        System.out.println("Something new");
     }
 }
